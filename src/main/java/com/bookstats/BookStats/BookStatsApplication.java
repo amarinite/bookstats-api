6ac711483bfec5a512837be3c3736c04db995ca5
Alba <albamrquz3@gmail.com>
2025-05-28 23:38:43 +0200

    first commit

---
+++ src/main/java/com/bookstats/BookStats/BookStatsApplication.java
@@ -0,0 +1,13 @@
+package com.bookstats.BookStats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStatsApplication.class, args);
+	}
+
+}
